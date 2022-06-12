5b941dea7958cd1cf51ed042c193156e550685b9
galenys <shivbhatia10@gmail.com>
2022-06-12 14:27:17 +0100

    add vaccination

--- test.py
+++ test.py
@@ -21,12 +21,14 @@ class Node:
         self.status = status
 
 class Simulation:
-    def __init__(self, G, N, inf_rate, rec_rate):
+    def __init__(self, G, N, inf_rate, rec_rate, vacc_inf_rate):
         self.nodeStatuses = [Status.SUS for _ in range(N)]
         self.G = G
         self.N = N
         self.inf_rate = inf_rate
         self.rec_rate = rec_rate
+        self.vacc_inf_rate = vacc_inf_rate
+        self.vaccinated = []
 
     def initialise_population(self, infected):
         for i in range(self.N):
@@ -35,11 +37,26 @@ class Simulation:
             else:
                 self.nodeStatuses[i] = Status.SUS
 
+    def no_vaccines(self):
+        self.vaccinated = []
+
+    def vaccinate_randomly(self, p):
+        self.vaccinated = random.sample(range(self.N), round(self.N * p))
+
+    def vaccinate_highest_degree_first(self, p):
+        self.vaccinated = list(range(self.N))
+        self.vaccinated.sort(reverse=True, key=self.get_degree)
+        self.vaccinated = self.vaccinated[:round(self.N * p)]
+
+    def get_degree(self, i):
+        return self.G.degree[i]
+
     def timestep(self):
         for node in self.G:
             if self.nodeStatuses[node] == Status.INF:
                 for neighbour in self.G.neighbors(node):
-                    if (random.random() < self.inf_rate and 
+                    rate = self.vacc_inf_rate if neighbour in self.vaccinated else self.inf_rate
+                    if (random.random() < rate and 
                             self.nodeStatuses[neighbour] == Status.SUS):
                         self.nodeStatuses[neighbour] = Status.INF
                 if random.random() < self.rec_rate:
@@ -67,9 +84,8 @@ class Simulation:
             else:
                 recovered += 1
         return [susceptible/self.N, infected/self.N, recovered/self.N]
-    
 
-    def disease_graph(self, infected_proportion):
+    def disease_graph(self, infected_proportion, should_display=False):
         self.initialise_population(infected=list(range(round(self.N*infected_proportion))))
         sus, inf, rec = [], [], []
         proportions = self.get_proportions()
@@ -81,14 +97,22 @@ class Simulation:
             self.timestep()
             proportions = self.get_proportions()
             timesteps += 1
-        X = np.linspace(0, timesteps, timesteps)
-        plt.plot(X, sus, label="Susceptible")
-        plt.plot(X, inf, label="Infected")
-        plt.plot(X, rec, label="Recovered")
-        plt.legend()
-        plt.ylim((0, 1))
-        plt.show()
+        if should_display:
+            X = np.linspace(0, timesteps, timesteps)
+            plt.plot(X, sus, label="Susceptible")
+            plt.plot(X, inf, label="Infected")
+            plt.plot(X, rec, label="Recovered")
+            plt.legend()
+            plt.ylim((0, 1))
+            plt.show()
         return proportions[2]
+
+    def average_disease_graph_recovered_rate(self, infected_proportion, iterations):
+        total = 0
+        for i in range(iterations):
+            total += self.disease_graph(infected_proportion=infected_proportion)
+            print(f"Iteration {i}")
+        return total / iterations
 
     def display_network(self):
         random_pos = nx.random_layout(self.G, seed=42)
@@ -104,15 +128,16 @@ class Simulation:
     #     self.timestep()
 
     # def continuous_display(self):
-    #     self.infect_list([i for i in range(20)])
+    #     self.initialise_population([i for i in range(20)])
     #     anim = animation.FuncAnimation(plt.gcf(), self.animate, frames=20, interval=20)
     #     plt.show()
 
 S = Simulation(
-        G=nx.gnp_random_graph(500, 0.01, seed=42),
-        # G=nx.scale_free_graph(500),
+        # G=nx.gnp_random_graph(500, 0.005, seed=42),
+        G=nx.scale_free_graph(500, seed=42).to_undirected(),
         N=500,
-        inf_rate=0.05,
-        rec_rate=0.1
+        inf_rate=0.1,
+        rec_rate=0.1,
+        vacc_inf_rate=0.001
         )
 
